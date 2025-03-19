230ce2491ef7643e455b36c23931439266a9dabd
VISMAH <maheshratta@gmail.com>
2025-03-19 22:43:29 +0530

    tailwind2

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-'>Tailwind test </h1>
+     <h1 className='bg-green-400 text-black p-4 rounded-3xl'>Tailwind test </h1>
+     <card/>
+     
+
     </>
   )
 }
